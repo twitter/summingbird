fa12370a122292a35c5f4eee7414004c69b7d4c1
Sam Ritchie <sritchie@twitter.com>
2013-01-07 12:20:10 -0800

    Adds rangedsource.

---
+++ src/main/scala/com/twitter/summingbird/source/RangedSource.scala
@@ -0,0 +1,28 @@
+package com.twitter.summingbird.source
+
+import cascading.flow.FlowDef
+import com.twitter.scalding.{ Mode, Mappable, DateRange, RichDate, Hours }
+
+/**
+ *  @author Oscar Boykin
+ *  @author Sam Ritchie
+ */
+
+// RangedSource extends ScaldingSource for use with date-aware
+// sources. Many of these can be found in scalding-commons. . All
+// sources that use this trait will automatically slop one hour behind
+// and one hours forward to account for the delay in log grouping.
+
+trait RangedSource[Event,Time] extends ScaldingSource[Event,Time] {
+  def dateOf(t: Time): RichDate
+  def rangedSource(range: DateRange): Mappable[Event]
+
+  override def source(lower: Time, upper: Time)
+  (implicit flow: FlowDef, mode: Mode) = {
+    // Source one hour behind and three hours forward to compensate
+    // for the delay introduced by Twitter's log-grouping.
+    val lo = dateOf(lower) - Hours(1)
+    val hi = dateOf(upper) + Hours(1)
+    rangedSource(DateRange(lo, hi))
+  }
+}
