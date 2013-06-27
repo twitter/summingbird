09669232cbad160a326c4c9851ade2a7c5b2b52f
Sam Ritchie <sritchie@twitter.com>
2013-06-27 16:18:52 -0700

    versioned store

--- summingbird-builder/src/main/scala/com/twitter/summingbird/scalding/store/VersionedStore.scala
+++ summingbird-builder/src/main/scala/com/twitter/summingbird/scalding/store/VersionedStore.scala
@@ -1,42 +1,42 @@
 /*
-Copyright 2013 Twitter, Inc.
+ Copyright 2013 Twitter, Inc.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 package com.twitter.summingbird.scalding.store
 
 import cascading.flow.FlowDef
 import com.twitter.bijection.Injection
-import com.twitter.scalding.{Dsl, Mode, TDsl, TypedPipe}
+import com.twitter.scalding.{Dsl, Hdfs, Mode, TDsl, TypedPipe}
 import com.twitter.scalding.commons.source.VersionedKeyValSource
 import com.twitter.summingbird.batch.BatchID
-import com.twitter.summingbird.scalding.ScaldingEnv
+import com.twitter.summingbird.scalding._
 import scala.util.control.Exception.allCatch
 
 /**
- * Scalding implementation of the batch read and write components
- * of a store that uses the VersionedKeyValSource from scalding-commons.
- *
- * @author Oscar Boykin
- * @author Sam Ritchie
- * @author Ashu Singhal
- */
+  * Scalding implementation of the batch read and write components
+  * of a store that uses the VersionedKeyValSource from scalding-commons.
+  *
+  * @author Oscar Boykin
+  * @author Sam Ritchie
+  * @author Ashu Singhal
+  */
 
 object VersionedStore {
-  def apply[Key, Value](rootPath: String, versionsToKeep: Int = VersionedKeyValSource.defaultVersionsToKeep)
-      (implicit injection: Injection[(Key, Value), (Array[Byte], Array[Byte])]) =
-    new VersionedStore[Key, Value](rootPath, versionsToKeep)
+  def apply[K, V](rootPath: String, versionsToKeep: Int = VersionedKeyValSource.defaultVersionsToKeep)
+    (implicit injection: Injection[(K, V), (Array[Byte], Array[Byte])]) =
+    new VersionedStore[K, V](rootPath, versionsToKeep)
 }
 
 // TODO: it looks like when we get the mappable/directory this happens
@@ -45,8 +45,8 @@ object VersionedStore {
 // meta-data and open the Mappable.
 // The source parameter is pass-by-name to avoid needing the hadoop
 // Configuration object when running the storm job.
-class VersionedStore[Key, Value](rootPath: String, versionsToKeep: Int)(
-    implicit injection: Injection[(Key, Value), (Array[Byte], Array[Byte])])  extends BatchStore[Key, Value] {
+class VersionedStore[K, V](rootPath: String, versionsToKeep: Int)(
+  implicit injection: Injection[(K, V), (Array[Byte], Array[Byte])])  extends BatchedScaldingStore[K, V] {
   import Dsl._
   import TDsl._
 
@@ -61,18 +61,24 @@ class VersionedStore[Key, Value](rootPath: String, versionsToKeep: Int)(
     VersionedKeyValSource(rootPath, sourceVersion=Some(v))
 
   override def readLatest(env: ScaldingEnv)(implicit fd: FlowDef, mode: Mode)
-      : Option[(BatchID, TypedPipe[(Key,Value)])] =
+      : Option[(BatchID, TypedPipe[(K, V)])] =
     prevUpperBound(env).map { case (bid, version) =>
       (bid, readVersion(version))
     }
 
-  /**
-    * Read the version aggregated up to the supplied batchID.
-    */
-  override def read(batchId: BatchID, env: ScaldingEnv)(implicit fd: FlowDef, mode: Mode) =
-    HDFSMetadata(env.config, rootPath)
-      .find { bstr: String => BatchID(bstr) == batchId }
-      .map { case (bstr, hdfsVersion) => readVersion(hdfsVersion.version) }
+  override def readLast(exclusiveUB: BatchID, mode: Mode): Try[(BatchID, FlowToPipe[(K, V)])] = {
+    mode match {
+      case Hdfs(_, conf) =>
+        HDFSMetadata(conf, rootPath)
+          .find { bstr: String => BatchID(bstr) == exclusiveUB }
+          .map { case (bstr, hdfsVersion) =>
+            val batch = BatchID(bstr)
+            val pipe = readVersion(hdfsVersion.version)
+            Right[List[FailureReason], T](BatchID(bstr) -> pipe.map { case (k, v) => (1L, k, v) })
+        }
+      case _ => Left()
+    }
+  }
 
   override def availableBatches(env: ScaldingEnv): Iterable[BatchID] =
     HDFSMetadata(env.config, rootPath)
@@ -80,6 +86,7 @@ class VersionedStore[Key, Value](rootPath: String, versionsToKeep: Int)(
       .map { case (batchString, _) => BatchID(batchString) }
 
   protected var sinkVersion: Option[Long] = None
+
   // Make sure we only allocate a version once
   def getSinkVersion(env: ScaldingEnv): Long =
     sinkVersion.getOrElse {
@@ -88,14 +95,14 @@ class VersionedStore[Key, Value](rootPath: String, versionsToKeep: Int)(
       v
     }
 
-  def write(env: ScaldingEnv, p: TypedPipe[(Key,Value)])
-  (implicit fd: FlowDef, mode: Mode) {
+  def write(env: ScaldingEnv, p: TypedPipe[(K, V)])
+    (implicit fd: FlowDef, mode: Mode) {
     p.toPipe((0,1))
       .write(VersionedKeyValSource(rootPath,
-          sourceVersion=None,
-          sinkVersion=Some(getSinkVersion(env)),
-          maxFailures=0,
-          versionsToKeep=versionsToKeep))
+        sourceVersion = None,
+        sinkVersion = Some(getSinkVersion(env)),
+        maxFailures = 0,
+        versionsToKeep = versionsToKeep))
   }
 
   def commit(batchID: BatchID, env: ScaldingEnv) {
