7a27023d83f3daf78f3b9b725a9b6b7b15a2f7d7
P. Oscar Boykin <oscar@twitter.com>
2013-11-12 16:18:14 -0800

    Fix indentation

--- summingbird-storm/src/main/scala/com/twitter/summingbird/storm/StormPlatform.scala
+++ summingbird-storm/src/main/scala/com/twitter/summingbird/storm/StormPlatform.scala
@@ -95,15 +95,15 @@ object Storm {
   def service[K, V](serv: => ReadableStore[K, V]): StormService[K, V] = StoreWrapper(() => serv)
 
   def toStormSource[T](spout: Spout[T],
-    defaultSourcePar: Option[Int] = None)(implicit timeOf: TimeExtractor[T]): StormSource[T] =
-      SpoutSource(spout.map(t => (timeOf(t), t)), defaultSourcePar.map(option.SpoutParallelism(_)))
+                       defaultSourcePar: Option[Int] = None)(implicit timeOf: TimeExtractor[T]): StormSource[T] =
+    SpoutSource(spout.map(t => (timeOf(t), t)), defaultSourcePar.map(option.SpoutParallelism(_)))
 
   implicit def spoutAsStormSource[T](spout: Spout[T])(implicit timeOf: TimeExtractor[T]): StormSource[T] =
     toStormSource(spout, None)(timeOf)
 
   def source[T](spout: Spout[T],
-    defaultSourcePar: Option[Int] = None)(implicit timeOf: TimeExtractor[T]): Producer[Storm, T] =
-      Producer.source[Storm, T](toStormSource(spout, defaultSourcePar))
+                defaultSourcePar: Option[Int] = None)(implicit timeOf: TimeExtractor[T]): Producer[Storm, T] =
+    Producer.source[Storm, T](toStormSource(spout, defaultSourcePar))
 
   implicit def spoutAsSource[T](spout: Spout[T])(implicit timeOf: TimeExtractor[T]): Producer[Storm, T] =
     source(spout, None)(timeOf)
