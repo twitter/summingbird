af30e34b5ba9a78bb3c544b9813400335823d8e2
Ian O Connell <ioconnell@twitter.com>
2013-12-03 11:49:42 -0800

    Fix a little WS

--- summingbird-storm/src/main/scala/com/twitter/summingbird/storm/Constants.scala
+++ summingbird-storm/src/main/scala/com/twitter/summingbird/storm/Constants.scala
@@ -46,7 +46,7 @@ object Constants {
   val DEFAULT_MONOID_IS_COMMUTATIVE = MonoidIsCommutative.default
   val DEFAULT_MAX_WAITING_FUTURES = MaxWaitingFutures(10)
   val DEFAULT_MAX_FUTURE_WAIT_TIME =  MaxFutureWaitTime(Duration.fromSeconds(60))
-val DEFAULT_FM_PREFER_LOCAL_DEPENDENCY =  PreferLocalDependency(false)
+  val DEFAULT_FM_PREFER_LOCAL_DEPENDENCY =  PreferLocalDependency(false)
   val DEFAULT_FLUSH_FREQUENCY =  FlushFrequency(Duration.fromSeconds(10))
   val DEFAULT_USE_ASYNC_CACHE = UseAsyncCache(false)
 }
