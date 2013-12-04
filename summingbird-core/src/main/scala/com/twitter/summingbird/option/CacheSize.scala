edcf108bfe2d43477684fab618b02bbb15d16c1a
Ian O Connell <ioconnell@twitter.com>
2013-12-04 11:52:57 -0800

    Add error checking to the cache size so small CacheSize doesn't throw an exception

--- summingbird-core/src/main/scala/com/twitter/summingbird/option/CacheSize.scala
+++ summingbird-core/src/main/scala/com/twitter/summingbird/option/CacheSize.scala
@@ -30,8 +30,14 @@ import scala.util.Random
  */
 
 case class CacheSize(lowerBound: Int, fuzz: Double = 0.2) extends java.io.Serializable {
+  private def getFuzz(base: Int): Int =
+    if((fuzz * base).toInt > 0)
+      Random.nextInt((fuzz * base).toInt)
+    else
+      0
+
   def size: Option[Int] =
     Some(lowerBound)
       .filter { _ > 0 }
-      .map { s => s + Random.nextInt((fuzz * s).toInt)  }
+      .map { s => s + getFuzz(s)  }
 }
