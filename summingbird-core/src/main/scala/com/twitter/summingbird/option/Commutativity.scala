8a86853705b83c46ae0570a3e213a67ddafeb9bf
Jonathan Coveney <jcoveney@twitter.com>
2014-02-12 11:31:16 -0800

    Make Commutative settings a case object

--- summingbird-core/src/main/scala/com/twitter/summingbird/option/Commutativity.scala
+++ summingbird-core/src/main/scala/com/twitter/summingbird/option/Commutativity.scala
@@ -18,15 +18,11 @@ package com.twitter.summingbird.option
 
 import java.io.Serializable
 
-trait BasicPrint {
-  override def toString = getClass.getName
-}
-
 // TODO: this functionality should be in algebird:
 // https://github.com/twitter/algebird/issues/128
-sealed trait Commutativity extends Serializable with BasicPrint
-object NonCommutative extends Commutativity
-object Commutative extends Commutativity
+sealed trait Commutativity extends Serializable
+case object NonCommutative extends Commutativity
+case object Commutative extends Commutativity
 
 /** A readable way to specify commutivity in a way
  * that works with the Class-based Options system.
