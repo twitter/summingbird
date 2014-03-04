586501a13c479710178a3b056137a1f673c6f926
Jonathan Coveney <jcoveney@twitter.com>
2014-03-04 11:53:57 -0800

    bump version

--- project/Build.scala
+++ project/Build.scala
@@ -22,7 +22,7 @@ object SummingbirdBuild extends Build {
 
   val sharedSettings = extraSettings ++ Seq(
     organization := "com.twitter",
-    version := "0.3.3",
+    version := "0.3.4rc1",
     scalaVersion := "2.9.3",
     crossScalaVersions := Seq("2.9.3", "2.10.0"),
     libraryDependencies ++= Seq(
@@ -125,12 +125,12 @@ object SummingbirdBuild extends Build {
   )
 
   val dfsDatastoresVersion = "1.3.4"
-  val bijectionVersion = "0.6.0"
-  val algebirdVersion = "0.3.1"
-  val scaldingVersion = "0.9.0rc4"
-  val storehausVersion = "0.8.0"
+  val bijectionVersion = "0.6.2"
+  val algebirdVersion = "0.5.0rc4"
+  val scaldingVersion = "0.9.0rc9"
+  val storehausVersion = "0.8.1rc1"
   val utilVersion = "6.3.8"
-  val chillVersion = "0.3.5"
+  val chillVersion = "0.3.6rc2"
   val tormentaVersion = "0.7.0"
 
   lazy val slf4jVersion = "1.6.6"
