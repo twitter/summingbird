2225595e3b169c6e7b33f9fc419b4f56486fbde9
Ian O Connell <ioconnell@twitter.com>
2014-07-13 22:31:05 -0700

    Bump the version correctly

--- project/Build.scala
+++ project/Build.scala
@@ -31,7 +31,7 @@ object SummingbirdBuild extends Build {
 
   val sharedSettings = extraSettings ++ Seq(
     organization := "com.twitter",
-    version := "0.5.0",
+    version := "0.5.1",
     scalaVersion := "2.9.3",
     crossScalaVersions := Seq("2.9.3", "2.10.4"),
     // To support hadoop 1.x
