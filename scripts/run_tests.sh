31bf87e5b4a3298945ce1df5e471e5d32c1f2744
Mansur Ashraf <mashraf@twitter.com>
2015-02-13 22:17:44 -0800

    bumped heap and permgen

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,6 +7,6 @@ PROGRESS_REPORTER_PID=$!
 time ./sbt ++$TRAVIS_SCALA_VERSION $TEST_TARGET/compile $TEST_TARGET/test:compile &> /dev/null
 kill -9 $PROGRESS_REPORTER_PID
 
-export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=128m -Xms256m -Xmx512m -Xss2m"
+export JVM_OPTS="-XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:ReservedCodeCacheSize=96m -XX:+TieredCompilation -XX:MaxPermSize=128m -Xms512m -Xmx2048m -Xss2m"
 
 ./sbt -Dlog4j.configuration=file://$TRAVIS_BUILD_DIR/project/travis-log4j.properties ++$TRAVIS_SCALA_VERSION $TEST_TARGET/test
