06a5777a3ffde3897f3be5f178a8b0d37fc9e4a5
Pankaj Gupta <pankaj.roark@gmail.com>
2016-08-18 11:49:24 -0700

    Modify comment

--- summingbird-core/src/main/scala/com/twitter/summingbird/planner/Node.scala
+++ summingbird-core/src/main/scala/com/twitter/summingbird/planner/Node.scala
@@ -101,7 +101,7 @@ case class SourceNode[P <: Platform[P]](override val members: List[Producer[P, _
  *                                    is the mapping of nodes to their unique names.
  *                                    Note that this name is different from the name used for
  *                                    applying named options.
- * @param nameToNode                  Reverse of above mapping. What is the node for a given name.
+ * @param nameToNode                  Reverse of above mapping. Mapping of names to nodes.
  * @param dependenciesOfM             Nodes this node immediately depends on. In other words,
  *                                    the parents of this node in node graph.
  * @param dependantsOfM               Nodes that immediately depend on the given node. In
