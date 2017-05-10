473517d36ca11fc142f3fbe1af4bec8bb94ef436
Timur Abishev <tabishev@twitter.com>
2017-05-10 15:42:54 -0700

    Fix algebird related issues based on https://github.com/twitter/summingbird/pull/715/

--- summingbird-batch/src/main/scala/com/twitter/summingbird/batch/TimeStamp.scala
+++ summingbird-batch/src/main/scala/com/twitter/summingbird/batch/TimeStamp.scala
@@ -56,13 +56,11 @@ object Timestamp {
   implicit val timestampSuccessible: Successible[Timestamp] = new Successible[Timestamp] {
     def next(old: Timestamp) = if (old.milliSinceEpoch != Long.MaxValue) Some(old.next) else None
     def ordering: Ordering[Timestamp] = Timestamp.orderingOnTimestamp
-    def partialOrdering = Timestamp.orderingOnTimestamp
   }
 
   implicit val timestampPredecessible: Predecessible[Timestamp] = new Predecessible[Timestamp] {
     def prev(old: Timestamp) = if (old.milliSinceEpoch != Long.MinValue) Some(old.prev) else None
     def ordering: Ordering[Timestamp] = Timestamp.orderingOnTimestamp
-    def partialOrdering = Timestamp.orderingOnTimestamp
   }
 
   // This is a right semigroup, that given any two Timestamps just take the one on the right.
